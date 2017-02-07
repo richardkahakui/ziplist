a6465318ea18dba7401da108009606b70c68bd98
RichardKahakui <rkahakui@hawaii.edu>
2017-02-06 23:00:08 -1000

    E27
    First changes to e27

---
+++ ziplist.js
@@ -0,0 +1,26 @@
+/*
+ * Created by richardkahakui on 2/5/17.
+ */
+/*
+*
+* E27: 5 problems (part 2)
+*/
+
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2, 3];
+
+function zipList(list1, list2){
+    const resultList = [];
+    for (let i = 0; i < list1.length; i++) {
+        resultList.push(list1[i], list2[i]);
+    }
+    return resultList;
+}
+
+//underscore version
+function zipListTheSimpleWay(list1,list2){
+  return _.flatten(_.zip(list1, list2));
+}
+console.log(zipList(testList1, testList2));
+
+console.log(zipListTheSimpleWay(testList1, testList2));
